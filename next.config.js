e3aa98a2ca536abb2e33ca74621a5bc6142e0271
juanmartin19-cloude <juanmartin19@gmail.com>
2026-05-14 22:59:52 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['supabase.co'],
   },
   env: {
     NEXT_PUBLIC_APP_NAME: 'INFRATEC CRM',
     NEXT_PUBLIC_APP_VERSION: '1.0.0',
-  },
-  async headers() {
-    return [
-      {
-        source: '/manifest.json',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/manifest+json',
-          },
-        ],
-      },
-    ];
   },
 }
 
